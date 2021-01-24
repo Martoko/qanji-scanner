cd9029455949362eb1efde9b1a4cd84e4e116ab2
martoko <martoko@martoko.dk>
2021-01-24 18:44:06 +0100

    Add libtorch

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
+#include <torch/torch.h>
+#include <iostream>
 #include <QApplication>
 #include <QPushButton>
 #include "scanner.h"
 
 int main(int argc, char *argv[]) {
+    torch::Tensor tensor = torch::rand({2, 3});
+    std::cout << tensor << std::endl;
+
     QApplication app(argc, argv);
 
     Scanner scanner;
