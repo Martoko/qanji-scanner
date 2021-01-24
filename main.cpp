ca2496c9916ab2866bdd937270193164847f8f06
martoko <martoko@martoko.dk>
2021-01-24 17:14:46 +0100

    Add some opencv test code

--- main.cpp
+++ main.cpp
@@ -2,7 +2,23 @@
 #include <QPushButton>
 #include "scanner.h"
 
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+void openCvTest() {
+    cv::Mat output = cv::Mat::zeros(120, 350, CV_8UC3);
+    cv::putText(output,
+                "Hello World :)",
+                cv::Point(15, 70),
+                cv::FONT_HERSHEY_PLAIN,
+                3,
+                cv::Scalar(0, 255, 0),
+                4);
+}
+
 int main(int argc, char *argv[]) {
+    openCvTest();
+
     QApplication app(argc, argv);
 
     Scanner scanner;
