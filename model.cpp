4b6d34023c52b316d3e94029709d042d3b6844cb
martoko <martoko@martoko.dk>
2021-01-24 22:19:57 +0100

    Fix model

--- model.cpp
+++ model.cpp
@@ -12,17 +12,17 @@ std::string Model::predict(QPixmap pixmap) {
     auto image = pixmap.scaled(w, h).toImage();
     for (int x = 0; x < w; ++x) {
         for (int y = 0; y < h; ++y) {
-            input[0][0][x][y] = image.pixelColor(x, y).redF();
-            input[0][1][x][y] = image.pixelColor(x, y).greenF();
-            input[0][2][x][y] = image.pixelColor(x, y).blueF();
+            input[0][0][y][x] = (image.pixelColor(x, y).redF() - normalizationMean[0]) / normalizationStd[0];
+            input[0][1][y][x] = (image.pixelColor(x, y).greenF() - normalizationMean[1]) / normalizationStd[1];
+            input[0][2][y][x] = (image.pixelColor(x, y).blueF() - normalizationMean[2]) / normalizationStd[2];
         }
     }
 
     return predict({torch::from_blob(input, {1, 3, w, h}, {torch::kFloat32})});
 }
 
-std::string Model::predict(std::vector<torch::jit::IValue> inputs) {
-    auto output = module.forward(inputs).toTensor();
+std::string Model::predict(std::vector<torch::jit::IValue> normalized_input) {
+    auto output = module.forward(normalized_input).toTensor();
     auto index = output.argmax(1).item<int>();
     return kanji[index];
 }
--- model.h
+++ model.h
@@ -1,7 +1,11 @@
 #pragma once
+
 #include <QPixmap>
+
 #undef slots
+
 #include <torch/script.h>
+
 #define slots Q_SLOTS
 
 class Model {
@@ -10,9 +14,11 @@ public:
 
     std::string predict(QPixmap pixmap);
 
-    std::string predict(std::vector<torch::jit::IValue> inputs);
+    std::string predict(std::vector<torch::jit::IValue> normalized_input);
 
 private:
+    float normalizationMean[3] = {0.63, 0.63, 0.63};
+    float normalizationStd[3] = {0.28, 0.28, 0.28};
     torch::jit::script::Module module;
 };
 
